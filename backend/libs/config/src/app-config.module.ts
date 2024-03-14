743600e559ebbd6fd73803116a9d0d84a67ca65d
Arina <arinakovalenco7@gmail.com>
2024-03-14 16:40:56 +0300

    Добавит пространство db для настроек

--- backend/libs/config/src/app-config.module.ts
+++ backend/libs/config/src/app-config.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './app.config';
+import mongoConfig from './mongo.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [appConfig],
+      load: [appConfig, mongoConfig],
     }),
   ],
 })
