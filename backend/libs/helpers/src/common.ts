c6354203537fd1802c4ca7f384184b8eb5d9d5b8
Arina <arinakovalenco7@gmail.com>
2024-03-14 20:17:12 +0300

    Добавит функцию getMongoConnectionString

---
+++ backend/libs/helpers/src/common.ts
@@ -0,0 +1,10 @@
+export function getMongoConnectionString({
+  username,
+  password,
+  host,
+  port,
+  databaseName,
+  authDatabase,
+}): string {
+  return `mongodb://${username}:${password}@${host}:${port}/${databaseName}?authSource=${authDatabase}`;
+}
