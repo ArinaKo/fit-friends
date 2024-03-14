9fb4f8bd2c818eea8ce9f26bc4e071b0ec8acc3c
Arina <arinakovalenco7@gmail.com>
2024-03-14 16:37:43 +0300

    Добавит пространство app с настройками приложения

--- backend/libs/config/src/app-config.module.ts
+++ backend/libs/config/src/app-config.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import appConfig from './app.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [],
+      load: [appConfig],
     }),
   ],
 })
-export class ConfigAppModule {}
+export class AppConfigModule {}
