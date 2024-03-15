bb6416e4d99957773675716878773dfefb2565f5
Arina <arinakovalenco7@gmail.com>
2024-03-15 13:15:52 +0300

    Добавит типы для запроса на тренировку

---
+++ backend/libs/types/src/request-status.enum.ts
@@ -0,0 +1,5 @@
+export enum RequestStatus {
+  Default = 'на рассмотрении',
+  Rejected = 'отклонён',
+  Accepted = 'принят',
+}
---
+++ backend/libs/types/src/request.interface.ts
@@ -0,0 +1,9 @@
+import { RequestStatus } from './request-status.enum';
+
+export interface Request {
+  userFromId: string;
+  userToId: string;
+  status: RequestStatus;
+  createdAt: Date;
+  updatedAt: Date;
+}
