cfabb672c9992aebcf3cc475c8132d66af0a186d
Arina <arinakovalenco7@gmail.com>
2024-03-18 21:13:24 +0300

    Добавит модель UserModel

---
+++ backend/src/users/user.model.ts
@@ -0,0 +1,113 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import {
+  AuthUser,
+  MetroStation,
+  UserLevel,
+  UserRole,
+  UserSex,
+  WorkoutDuration,
+  WorkoutType,
+} from '@app/types';
+
+@Schema({
+  collection: 'accounts',
+  timestamps: true,
+})
+export class UserModel extends Document implements AuthUser {
+  @Prop({
+    required: true,
+  })
+  public avatar: string;
+
+  @Prop()
+  public dateOfBirth?: Date;
+
+  @Prop({
+    required: true,
+    unique: true,
+  })
+  public email: string;
+
+  @Prop({
+    required: true,
+  })
+  public name: string;
+
+  @Prop({
+    required: true,
+  })
+  public passwordHash: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserRole,
+    default: UserRole.Default,
+  })
+  public role: UserRole;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserSex,
+  })
+  public sex: UserSex;
+
+  @Prop({
+    required: true,
+  })
+  public description: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: MetroStation,
+  })
+  public location: MetroStation;
+
+  @Prop({})
+  public backgroundImage?: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: UserLevel,
+  })
+  public level: UserLevel;
+
+  @Prop({
+    required: true,
+    type: () => [String],
+    enum: WorkoutType,
+  })
+  public workoutTypes: WorkoutType[];
+
+  @Prop({
+    required: true,
+  })
+  public isReady: boolean;
+
+  @Prop()
+  public createdAt: Date;
+
+  @Prop()
+  public certificate?: string;
+
+  @Prop()
+  public achievements?: string;
+
+  @Prop()
+  public caloriesToLose?: number;
+
+  @Prop()
+  public caloriesPerDay?: number;
+
+  @Prop({
+    type: String,
+    enum: WorkoutDuration,
+  })
+  public timeForWorkout?: WorkoutDuration;
+}
+
+export const UserSchema = SchemaFactory.createForClass(UserModel);
--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModel, UserSchema } from './user.model';
 
-@Module({})
+@Module({
+  imports: [MongooseModule.forFeature([
+    { name: UserModel.name, schema: UserSchema }
+  ])],
+})
 export class UsersModule {}
