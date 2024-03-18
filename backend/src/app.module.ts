625bd6f2ad0e638d7bf79c3b71ae5a813b807534
Arina <arinakovalenco7@gmail.com>
2024-03-18 20:02:20 +0300

    Добавит модуль users

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppConfigModule, getMongooseOptions } from '@app/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     AppConfigModule, 
-    MongooseModule.forRootAsync(getMongooseOptions())
+    MongooseModule.forRootAsync(getMongooseOptions()),
+    UsersModule,
   ],
   controllers: [],
   providers: [],
