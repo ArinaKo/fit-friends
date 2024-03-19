fe1e4f137872955cd2b990b8764aff4e8c9adafa
Arina <arinakovalenco7@gmail.com>
2024-03-19 15:27:17 +0300

    Добавит метод регистрации в UserService

--- backend/src/users/user.const.ts
+++ backend/src/users/user.const.ts
@@ -1,1 +1,5 @@
 export const SALT_ROUNDS = 10;
+
+export const UserMessage = {
+  Exists: 'User with this email exists',
+};
--- backend/src/users/user.service.ts
+++ backend/src/users/user.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { UserRepository } from './user.repository';
+import { UserEntity } from './user.entity';
+import { CreateUserDto } from './dto/index';
+import dayjs from 'dayjs';
+import { UserMessage } from './user.const';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  public async register(dto: CreateUserDto) {
+    const {
+      email,
+      name,
+      avatar,
+      sex,
+      dateOfBirth,
+      role,
+      description,
+      location,
+      backgroundImage,
+      level,
+      workoutTypes,
+      isReady,
+      certificate,
+      achievements,
+      caloriesToLose,
+      caloriesPerDay,
+      timeForWorkout,
+      password
+    } = dto;
+
+    const existedUser = await this.userRepository.findByEmail(email);
+
+    if (existedUser) {
+      throw new ConflictException(UserMessage.Exists);
+    }
+
+    const newUser = {
+      email,
+      name,
+      avatar,
+      sex,
+      dateOfBirth: dateOfBirth ? dayjs(dateOfBirth).toDate() : undefined,
+      role,
+      description,
+      location,
+      backgroundImage: backgroundImage ?? avatar,
+      level,
+      workoutTypes,
+      isReady,
+      certificate,
+      achievements,
+      caloriesToLose,
+      caloriesPerDay,
+      timeForWorkout,
+      passwordHash: '',
+      createdAt: new Date()
+    };
+    const userEntity = await new UserEntity(newUser).setPassword(password);
+
+    return this.userRepository.save(userEntity);
+  }
+}
