7842472969e81e0f99af96dbac65c292233155e5
Arina <arinakovalenco7@gmail.com>
2024-03-19 21:22:59 +0300

    Добавить обработчики маршрутов регистрации и авторизации

--- backend/src/users/user.controller.ts
+++ backend/src/users/user.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto, LoginUserDto } from './dto';
+import { fillDto } from '@app/helpers';
+import { FullUserRdo, LoggedUserRdo } from './rdo';
 
 @Controller('users')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post('register')
+  public async create(@Body() dto: CreateUserDto) {
+    const newUser = await this.userService.register(dto);
+    return fillDto(FullUserRdo, newUser.toPOJO());
+  }
+
+  @Post('login')
+  public async login(@Body() dto: LoginUserDto) {
+    const verifiedUser = await this.userService.verifyUser(dto);
+    return fillDto(LoggedUserRdo, verifiedUser.toPOJO());
+  }
+}
