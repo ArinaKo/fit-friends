b2ae158a1803df4b5b4d61ff21bcd12332d742aa
Arina <arinakovalenco7@gmail.com>
2024-03-20 22:00:10 +0300

    Дополнит метод регистрации в UserService

--- backend/src/users/user.service.ts
+++ backend/src/users/user.service.ts
@@ -1,9 +1,20 @@
-import { ConflictException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { UserEntity } from './user.entity';
-import { CreateUserDto, LoginUserDto } from './dto/index';
+import {
+  CreateCoachUserDto,
+  CreateDefaultUserDto,
+  CreateUserDto,
+  LoginUserDto,
+} from './dto/index';
 import dayjs from 'dayjs';
 import { UserMessage } from './user.const';
+import { UserRole } from '@app/types';
 
 @Injectable()
 export class UserService {
@@ -23,12 +34,7 @@ export class UserService {
       level,
       workoutTypes,
       isReady,
-      certificate,
-      achievements,
-      caloriesToLose,
-      caloriesPerDay,
-      timeForWorkout,
-      password
+      password,
     } = dto;
 
     const existedUser = await this.userRepository.findByEmail(email);
@@ -50,28 +56,43 @@ export class UserService {
       level,
       workoutTypes,
       isReady,
-      certificate,
-      achievements,
-      caloriesToLose,
-      caloriesPerDay,
-      timeForWorkout,
       passwordHash: '',
-      createdAt: new Date()
+      createdAt: new Date(),
     };
-    const userEntity = await new UserEntity(newUser).setPassword(password);
+
+    const defaultUserInfo =
+      role === UserRole.Default && dto instanceof CreateDefaultUserDto
+        ? {
+            caloriesToLose: dto.caloriesToLose,
+            caloriesPerDay: dto.caloriesPerDay,
+            timeForWorkout: dto.timeForWorkout,
+          }
+        : {};
+
+    const coachUserInfo =
+      role === UserRole.Coach && dto instanceof CreateCoachUserDto
+        ? {
+            certificate: dto.certificate,
+            achievements: dto.achievements,
+          }
+        : {};
+
+    const userEntity = await new UserEntity(
+      Object.assign(newUser, defaultUserInfo, coachUserInfo),
+    ).setPassword(password);
 
     return this.userRepository.save(userEntity);
   }
 
   public async verifyUser(dto: LoginUserDto) {
-    const {email, password} = dto;
+    const { email, password } = dto;
     const existUser = await this.userRepository.findByEmail(email);
 
     if (!existUser) {
       throw new NotFoundException(UserMessage.NotFound);
     }
 
-    if (!await existUser.comparePassword(password)) {
+    if (!(await existUser.comparePassword(password))) {
       throw new UnauthorizedException(UserMessage.PasswordWrong);
     }
 
