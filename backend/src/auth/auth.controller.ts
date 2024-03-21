65376cd497d45aeb796378414fb68f4affca3e5b
Arina <arinakovalenco7@gmail.com>
2024-03-21 21:51:27 +0300

    Добавит JwtRefreshGuard и обработчик маршрута refresh

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,4 +1,12 @@
-import { Body, Controller, HttpStatus, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { fillDto } from '@app/helpers';
@@ -9,6 +17,7 @@ import { CreateUserDtoListing } from './auth.const';
 import { Public } from '@app/core/decorators/public.decorator';
 import { UserEntity } from '../users/user.entity';
 import { LocalAuthGuard } from './guards/local-auth.guard';
+import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
 
 interface RequestWithUser {
   user: UserEntity;
@@ -26,7 +35,9 @@ export class AuthController {
     description: 'The new user has been successfully created.',
   })
   @Post('register')
-  public async create(@Body(new UserDtoValidationPipe(CreateUserDtoListing)) dto: CreateUserDto) {
+  public async create(
+    @Body(new UserDtoValidationPipe(CreateUserDtoListing)) dto: CreateUserDto,
+  ) {
     const newUser = await this.authService.register(dto);
     return fillDto(FullUserRdo, newUser.toPOJO());
   }
@@ -46,5 +57,16 @@ export class AuthController {
   public async login(@Req() { user }: RequestWithUser) {
     const userToken = await this.authService.createUserToken(user);
     return fillDto(LoggedUserRdo, { ...user.toPOJO(), ...userToken });
+  }
+
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Get a new access/refresh tokens',
+  })
+  @UseGuards(JwtRefreshGuard)
+  @Post('refresh')
+  @HttpCode(HttpStatus.OK)
+  public async refreshToken(@Req() { user }: RequestWithUser) {
+    return this.authService.createUserToken(user);
   }
 }
