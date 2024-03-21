2d25946444d3bf1666140208bab0b6a73a59ce3b
Arina <arinakovalenco7@gmail.com>
2024-03-21 16:52:21 +0300

    Добавит настройки для JWT

--- backend/libs/config/src/app-config.module.ts
+++ backend/libs/config/src/app-config.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import appConfig from './app.config';
 import mongoConfig from './mongo.config';
+import jwtConfig from './jwt.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       cache: true,
-      load: [appConfig, mongoConfig],
+      load: [appConfig, mongoConfig, jwtConfig],
     }),
   ],
 })
---
+++ backend/libs/config/src/jwt.config.ts
@@ -0,0 +1,32 @@
+import { registerAs } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export interface JWTConfig {
+  accessTokenSecret: string;
+  accessTokenExpiresIn: string;
+}
+
+const validationSchema = Joi.object({
+  accessTokenSecret: Joi.string().required(),
+  accessTokenExpiresIn: Joi.string().required(),
+});
+
+
+function validateConfig(config: JWTConfig): void {
+  const { error } = validationSchema.validate(config, { abortEarly: true });
+  if (error) {
+    throw new Error(`[Account JWTConfig Validation Error]: ${error.message}`);
+  }
+}
+
+function getConfig(): JWTConfig {
+  const config: JWTConfig = {
+    accessTokenSecret: process.env.JWT_ACCESS_TOKEN_SECRET!,
+    accessTokenExpiresIn: process.env.JWT_ACCESS_TOKEN_EXPIRES_IN!,
+  };
+
+  validateConfig(config);
+  return config;
+}
+
+export default registerAs('jwt', getConfig);
