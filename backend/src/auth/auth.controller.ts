a49f6d86a1d56caacc8015ef470c522454a5bcb2
Arina <arinakovalenco7@gmail.com>
2024-03-22 23:46:35 +0300

    Обновит логику публичности маршрутов

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -19,9 +19,11 @@ import { CreateUserDtoListing } from './auth.const';
 import { Public } from '@app/core/decorators/public.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { JwtRefreshGuard } from './guards/jwt-refresh.guard';
+import { NotAuthGuard } from './guards/not-auth.guard';
 import { UserService } from 'src/users/user.service';
 import { RefreshTokenService } from 'src/refresh-token/refresh-token.service';
 import { RequestWithUser } from './requests/request-with-user.interface';
+import { RequestWithTokenPayload } from './requests/request-with-token-payload.interface';
 import { RequestWithRefreshTokenPayload } from './requests/request-with-refresh-token-payload.interface';
 
 @ApiTags('auth')
@@ -38,6 +40,8 @@ export class AuthController {
     status: HttpStatus.CREATED,
     description: 'The new user has been successfully created.',
   })
+  @Public()
+  @UseGuards(NotAuthGuard)
   @Post('register')
   public async create(
     @Body(new UserDtoValidationPipe(CreateUserDtoListing)) dto: CreateUserDto,
@@ -55,7 +59,8 @@ export class AuthController {
     status: HttpStatus.UNAUTHORIZED,
     description: 'Password or Login is wrong.',
   })
-  @UseGuards(LocalAuthGuard)
+  @Public()
+  @UseGuards(NotAuthGuard, LocalAuthGuard)
   @Post('login')
   public async login(@Req() { user }: RequestWithUser) {
     const userToken = await this.authService.createUserToken(user);
@@ -81,6 +86,8 @@ export class AuthController {
     status: HttpStatus.OK,
     description: 'User has been successfully logged out.',
   })
+  @Public()
+  @UseGuards(JwtRefreshGuard)
   @Delete('logout')
   public async logout(@Req() { payload }: RequestWithRefreshTokenPayload) {
     const user = await this.refreshTokenService.deleteRefreshSession(payload.tokenId);
@@ -90,6 +97,7 @@ export class AuthController {
     status: HttpStatus.OK,
     description: 'Get a new access/refresh tokens',
   })
+  @Public()
   @UseGuards(JwtRefreshGuard)
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
---
+++ backend/src/auth/guards/not-auth.guard.ts
@@ -0,0 +1,24 @@
+import {
+  ExecutionContext,
+  ForbiddenException,
+  Injectable,
+  CanActivate,
+} from '@nestjs/common';
+import { Request } from 'express';
+
+@Injectable()
+export class NotAuthGuard implements CanActivate {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(request);
+    if (token) {
+      throw new ForbiddenException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers.authorization?.split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
