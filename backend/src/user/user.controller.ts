0e89bccc3d70c934fa994e5013b8422a4f45ad54
Arina <arinakovalenco7@gmail.com>
2024-03-23 17:33:55 +0300

    Добавить метод и обработчик маршрута для обновления пользователя

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
+import { Body, Controller, Get, HttpStatus, Param, Patch } from '@nestjs/common';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserService } from './user.service';
 import { fillDto } from '@app/helpers';
 import { FullUserRdo } from './rdo';
-import { MongoIdValidationPipe } from '@app/core';
+import { MongoIdValidationPipe, UserDtoValidationPipe } from '@app/core';
+import { UpdateUserDto } from './dto';
+import { AuthUserRdo } from 'src/auth/rdo';
+import { UpdateUserDtoListing } from './user.const';
 
 @ApiTags('users')
 @Controller('users')
@@ -15,9 +18,20 @@ export class UserController {
     status: HttpStatus.OK,
     description: 'User found'
   })
-  @Get(':userId')
+  @Get('/:userId')
   public async show(@Param('userId', MongoIdValidationPipe) id: string) {
     const existUser = await this.userService.getUserById(id);
     return fillDto(FullUserRdo, existUser.toPOJO());
+  }
+
+  @ApiResponse({
+    type: AuthUserRdo,
+    status: HttpStatus.OK,
+    description: 'User`s info has been successfully updated'
+  })
+  @Patch('/:userId')
+  public async update(@Param('userId', MongoIdValidationPipe) id: string, @Body(new UserDtoValidationPipe(UpdateUserDtoListing)) dto: UpdateUserDto) {
+    const updatedUser = await this.userService.updateUser(id, dto);
+    return fillDto(AuthUserRdo, updatedUser.toPOJO());
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -1,27 +1,55 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { UserRepository } from './user.repository';
+import { UpdateUserDto } from './dto';
+import { UserEntity } from './user.entity';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
 
-  public async getUserByEmail(email: string) {
-    const existUser = await this.userRepository.findByEmail(email);
+  public async getUserByEmail(email: string): Promise<UserEntity> {
+    const existsUser = await this.userRepository.findByEmail(email);
 
-    if (!existUser) {
+    if (!existsUser) {
       throw new NotFoundException(`User with email ${email} not found`);
     }
 
-    return existUser;
+    return existsUser;
   }
-  
-  public async getUserById(userId: string) {
-    const existUser = await this.userRepository.findById(userId);
 
-    if (!existUser) {
+  public async getUserById(userId: string): Promise<UserEntity> {
+    const existsUser = await this.userRepository.findById(userId);
+
+    if (!existsUser) {
       throw new NotFoundException(`User with id ${userId} not found`);
     }
 
-    return existUser;
+    return existsUser;
+  }
+
+  public async updateUser(
+    userId: string,
+    dto: UpdateUserDto,
+  ): Promise<UserEntity> {
+    const existsUser = await this.userRepository.findById(userId);
+
+    if (!existsUser) {
+      throw new NotFoundException(`User with id ${userId} not found`);
+    }
+
+    let hasChanges = false;
+
+    for (const [key, value] of Object.entries(dto)) {
+      if (value !== undefined && existsUser[key] !== value) {
+        existsUser[key] = value;
+        hasChanges = true;
+      }
+    }
+
+    if (!hasChanges) {
+      return existsUser;
+    }
+
+    return this.userRepository.update(userId, existsUser);
   }
 }
