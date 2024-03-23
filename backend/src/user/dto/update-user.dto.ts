1641723468c6a9dd9a27986913ac51a5d3c55bb8
Arina <arinakovalenco7@gmail.com>
2024-03-23 17:32:48 +0300

    Добавит UpdateUserDto

---
+++ backend/src/user/dto/update-user.dto.ts
@@ -0,0 +1,196 @@
+import {
+  MetroStation,
+  UserLevel,
+  UserSex,
+  WorkoutDuration,
+  WorkoutType,
+} from '@app/types';
+import { ApiPropertyOptional } from '@nestjs/swagger';
+import {
+  IsString,
+  ArrayMaxSize,
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  Length,
+  IsNumber,
+  Min,
+  Max,
+  IsOptional,
+  IsISO8601,
+} from 'class-validator';
+import {
+  CaloriesValue,
+  MAX_WORKOUTS_TYPES,
+  UserAchievementsLength,
+  UserDescriptionLength,
+  UserNameLength,
+} from 'src/const';
+import { UserValidationMessage } from '@app/messages';
+import { Expose } from 'class-transformer';
+
+class BaseUserDto {
+  @ApiPropertyOptional({
+    description: 'User avatar url',
+    example: 'image.jpg',
+  })
+  @IsOptional()
+  @Expose()
+  public avatar?: string;
+
+  @ApiPropertyOptional({
+    description: 'User birth date',
+    example: '1981-03-12',
+  })
+  @IsOptional()
+  @IsISO8601()
+  @Expose()
+  public dateOfBirth?: Date;
+
+  @ApiPropertyOptional({
+    description: 'User name',
+    example: 'John',
+  })
+  @IsOptional()
+  @IsString()
+  @Length(UserNameLength.Min, UserNameLength.Max, {
+    message: UserValidationMessage.name.length,
+  })
+  @Expose()
+  public name?: string;
+
+  @ApiPropertyOptional({
+    description: 'User sex',
+    example: 'мужской',
+  })
+  @IsOptional()
+  @IsEnum(UserSex, { message: UserValidationMessage.sex.invalidFormat })
+  @Expose()
+  public sex?: UserSex;
+
+  @ApiPropertyOptional({
+    description: 'User description',
+    example: 'Описание пользователя текстом',
+  })
+  @IsOptional()
+  @IsString()
+  @Length(UserDescriptionLength.Min, UserDescriptionLength.Max, {
+    message: UserValidationMessage.description.length,
+  })
+  @Expose()
+  public description?: string;
+
+  @ApiPropertyOptional({
+    description: 'User location - metro station',
+    example: 'Пионерская',
+  })
+  @IsOptional()
+  @IsEnum(MetroStation, {
+    message: UserValidationMessage.location.invalidFormat,
+  })
+  @Expose()
+  public location?: MetroStation;
+
+  @ApiPropertyOptional({
+    description: 'User image for background',
+    example: 'background-image.png',
+  })
+  @IsOptional()
+  @IsString()
+  @Expose()
+  public backgroundImage?: string;
+
+  @ApiPropertyOptional({
+    description: 'User level',
+    example: 'новичок',
+  })
+  @IsOptional()
+  @IsEnum(UserLevel, { message: UserValidationMessage.level.invalidFormat })
+  @Expose()
+  public level?: UserLevel;
+
+  @ApiPropertyOptional({
+    description: 'User`s workouts types',
+    example: 'йога, бег',
+  })
+  @IsOptional()
+  @IsArray()
+  @ArrayMaxSize(MAX_WORKOUTS_TYPES, {
+    message: UserValidationMessage.workoutsTypes.length,
+  })
+  @IsEnum(WorkoutType, {
+    each: true,
+    message: UserValidationMessage.workoutsTypes.invalidItems,
+  })
+  @Expose()
+  public workoutTypes?: WorkoutType[];
+
+  @ApiPropertyOptional({
+    description: 'Is user ready for workout?',
+    example: 'true',
+  })
+  @IsOptional()
+  @IsBoolean()
+  @Expose()
+  public isReady?: boolean;
+}
+
+export class UpdateDefaultUserDto extends BaseUserDto {
+  @ApiPropertyOptional({
+    description: 'Calories to lose',
+    example: '3200',
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(CaloriesValue.Min, { message: UserValidationMessage.calories.value })
+  @Max(CaloriesValue.Max, { message: UserValidationMessage.calories.value })
+  @Expose()
+  public caloriesToLose?: number;
+
+  @ApiPropertyOptional({
+    description: 'Calories to lose per day',
+    example: '1000',
+  })
+  @IsOptional()
+  @IsNumber()
+  @Min(CaloriesValue.Min, { message: UserValidationMessage.calories.value })
+  @Max(CaloriesValue.Max, { message: UserValidationMessage.calories.value })
+  @Expose()
+  public caloriesPerDay?: number;
+
+  @ApiPropertyOptional({
+    description: 'User`s preferable workout duration',
+    example: '10-30 мин',
+  })
+  @IsOptional()
+  @IsEnum(WorkoutDuration, {
+    message: UserValidationMessage.timeForWorkout.invalidFormat,
+  })
+  @Expose()
+  public timeForWorkout?: WorkoutDuration;
+}
+
+export class UpdateCoachUserDto extends BaseUserDto {
+  @ApiPropertyOptional({
+    description: 'Coach certificate',
+    example: 'certificate.jpg',
+  })
+  @IsOptional()
+  @IsString()
+  @Expose()
+  public certificate?: string;
+
+  @ApiPropertyOptional({
+    description: 'User`s achievements',
+    example: 'Мой список достижений',
+  })
+  @IsOptional()
+  @IsString()
+  @Length(UserAchievementsLength.Min, UserAchievementsLength.Max, {
+    message: UserValidationMessage.achievements.length,
+  })
+  @Expose()
+  public achievements?: string;
+}
+
+export type UpdateUserDto = UpdateCoachUserDto | UpdateDefaultUserDto;
--- backend/src/user/user.const.ts
+++ backend/src/user/user.const.ts
@@ -1,1 +1,9 @@
+import { UserRole } from '@app/types';
+import { UpdateCoachUserDto, UpdateDefaultUserDto } from './dto';
+
 export const SALT_ROUNDS = 10;
+
+export const UpdateUserDtoListing = {
+  [UserRole.Default]: UpdateDefaultUserDto,
+  [UserRole.Coach]: UpdateCoachUserDto,
+};
