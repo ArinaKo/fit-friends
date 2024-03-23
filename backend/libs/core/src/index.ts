819316fa6340cfa6dee2988ca07bfd4ebe41b31b
Arina <arinakovalenco7@gmail.com>
2024-03-23 15:48:29 +0300

    Добавит пользовательский пайп MongoIdValidationPipe

--- backend/libs/core/src/index.ts
+++ backend/libs/core/src/index.ts
@@ -2,3 +2,4 @@ export * from './repository/entity.interface';
 export * from './repository/repository.interface';
 export * from './repository/base-mongo.repository';
 export * from './pipes/user-dto-validation.pipe';
+export * from './pipes/mongo-id-validation.pipe';
