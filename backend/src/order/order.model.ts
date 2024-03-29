089cabd97b9188292656ad698539515aca2ce12d
Arina <arinakovalenco7@gmail.com>
2024-03-29 16:29:51 +0300

    Добавит OrderModel

---
+++ backend/src/order/order.model.ts
@@ -0,0 +1,46 @@
+import { Order, OrderType, PaymentType } from '@app/types';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export class OrderModel extends Document implements Order {
+  @Prop({
+    required: true,
+  })
+  public userId: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: OrderType,
+  })
+  public type: OrderType;
+
+  @Prop({
+    required: true,
+  })
+  public workoutId: string;
+
+  @Prop({
+    required: true,
+  })
+  public workoutPrice: number;
+
+  @Prop({
+    required: true,
+  })
+  public count: number;
+
+  @Prop({
+    required: true,
+  })
+  public totalPrice: number;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: PaymentType,
+  })
+  public paymentType: PaymentType;
+}
+
+export const OrderSchema = SchemaFactory.createForClass(OrderModel);
--- backend/src/order/order.module.ts
+++ backend/src/order/order.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { OrderController } from './order.controller';
 import { OrderService } from './order.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { OrderModel, OrderSchema } from './order.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: OrderModel.name, schema: OrderSchema }]),
+  ],
   controllers: [OrderController],
-  providers: [OrderService]
+  providers: [OrderService],
 })
 export class OrderModule {}
