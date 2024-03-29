aa6ae33760f12153be243280f5dca4c037753cfd
Arina <arinakovalenco7@gmail.com>
2024-03-29 16:25:11 +0300

    Добавит OrderEntity

---
+++ backend/src/order/order.entity.ts
@@ -0,0 +1,45 @@
+import { Entity } from '@app/core';
+import { Order, OrderType, PaymentType } from '@app/types';
+
+export class OrderEntity implements Order, Entity<string> {
+  public id?: string;
+  public userId: string;
+  public type: OrderType;
+  public workoutId: string;
+  public workoutPrice: number;
+  public count: number;
+  public totalPrice: number;
+  public paymentType: PaymentType;
+
+  constructor(Order: Order) {
+    this.populate(Order);
+  }
+
+  public toPOJO() {
+    return {
+      id: this.id,
+      userId: this.userId,
+      type: this.type,
+      workoutId: this.workoutId,
+      workoutPrice: this.workoutPrice,
+      count: this.count,
+      totalPrice: this.totalPrice,
+      paymentType: this.paymentType,
+    };
+  }
+
+  public populate(data: Order): void {
+    this.id = data.id;
+    this.userId = data.userId;
+    this.type = data.type;
+    this.workoutId = data.workoutId;
+    this.workoutPrice = data.workoutPrice;
+    this.count = data.count;
+    this.totalPrice = data.totalPrice;
+    this.paymentType = data.paymentType;
+  }
+
+  static fromObject(data: Order): OrderEntity {
+    return new OrderEntity(data);
+  }
+}
