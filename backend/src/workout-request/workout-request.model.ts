41be4a3845ea9edc90ab72c04bac5c5e501148df
Arina <arinakovalenco7@gmail.com>
2024-04-01 22:50:17 +0300

    Добавит WorkoutRequestModel

---
+++ backend/src/workout-request/workout-request.model.ts
@@ -0,0 +1,28 @@
+import { Document } from 'mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { RequestStatus, WorkoutRequest } from '@app/types';
+
+@Schema({
+  collection: 'workout-requests',
+  timestamps: true,
+})
+export class WorkoutRequestModel extends Document implements WorkoutRequest {
+  @Prop({
+    required: true,
+  })
+  public userFromId: string;
+
+  @Prop({
+    required: true,
+  })
+  public userToId: string;
+
+  @Prop({
+    required: true,
+    type: String,
+    enum: RequestStatus,
+  })
+  public status: RequestStatus;
+}
+
+export const WorkoutRequestSchema = SchemaFactory.createForClass(WorkoutRequestModel);
--- backend/src/workout-request/workout-request.module.ts
+++ backend/src/workout-request/workout-request.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { WorkoutRequestController } from './workout-request.controller';
 import { WorkoutRequestService } from './workout-request.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import {
+  WorkoutRequestModel,
+  WorkoutRequestSchema,
+} from './workout-request.model';
 
 @Module({
-
+  imports: [
+    MongooseModule.forFeature([
+      { name: WorkoutRequestModel.name, schema: WorkoutRequestSchema },
+    ]),
+  ],
   controllers: [WorkoutRequestController],
   providers: [WorkoutRequestService],
 })
