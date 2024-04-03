8a3b0ceae3566a57f61368a0178ff6cdd4c3b94b
Arina <arinakovalenco7@gmail.com>
2024-04-03 23:11:20 +0300

    Добавит метод для сохранения информации о файлах в бд

--- backend/src/file-vault/file-vault.service.ts
+++ backend/src/file-vault/file-vault.service.ts
@@ -1,5 +1,5 @@
 import 'multer';
-import { Inject, Injectable, Logger } from '@nestjs/common';
+import { Inject, Injectable, Logger, NotFoundException } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
 import { ensureDir } from 'fs-extra';
 import { writeFile } from 'node:fs/promises';
@@ -9,6 +9,8 @@ import { extension } from 'mime-types';
 import appConfig from '@app/config/app.config';
 import * as dayjs from 'dayjs';
 import { StoredFile } from '@app/types';
+import { FileEntity } from './file.entity';
+import { FileVaultRepository } from './file-vault.repository';
 
 @Injectable()
 export class FileVaultService {
@@ -18,6 +20,7 @@ export class FileVaultService {
   constructor(
     @Inject(appConfig.KEY)
     private readonly appOptions: ConfigType<typeof appConfig>,
+    private readonly fileVaultRepository: FileVaultRepository,
   ) {}
 
   private getUploadDirectoryPath(): string {
@@ -59,5 +62,29 @@ export class FileVaultService {
       this.logger.error(`Error while saving file: ${error.message}`);
       throw new Error(`Can't save file`);
     }
+  }
+
+  public async saveFile(file: Express.Multer.File): Promise<FileEntity> {
+    const storedFile = await this.writeFile(file);
+    const fileEntity = FileEntity.fromObject({
+      hashName: storedFile.filename,
+      mimetype: file.mimetype,
+      originalName: file.originalname,
+      path: storedFile.path,
+      size: file.size,
+      subDirectory: storedFile.subDirectory,
+    });
+
+    return this.fileVaultRepository.save(fileEntity);
+  }
+
+  public async getFile(fileId: string): Promise<FileEntity> {
+    const existFile = await this.fileVaultRepository.findById(fileId);
+
+    if (! existFile) {
+      throw new NotFoundException(`File with ${fileId} not found.`);
+    }
+
+    return existFile;
   }
 }
