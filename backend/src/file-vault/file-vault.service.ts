c216afb77ba67403b93f87b56a292f4ad00eac79
Arina <arinakovalenco7@gmail.com>
2024-04-03 22:59:01 +0300

    Добавит метод для сохранения файлов

--- backend/src/file-vault/file-vault.service.ts
+++ backend/src/file-vault/file-vault.service.ts
@@ -1,4 +1,63 @@
-import { Injectable } from '@nestjs/common';
+import 'multer';
+import { Inject, Injectable, Logger } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { ensureDir } from 'fs-extra';
+import { writeFile } from 'node:fs/promises';
+import { join } from 'node:path';
+import { randomUUID } from 'node:crypto';
+import { extension } from 'mime-types';
+import appConfig from '@app/config/app.config';
+import * as dayjs from 'dayjs';
+import { StoredFile } from '@app/types';
 
 @Injectable()
-export class FileVaultService {}
+export class FileVaultService {
+  private readonly logger = new Logger(FileVaultService.name);
+  private readonly DATE_FORMAT = 'YYYY MM';
+
+  constructor(
+    @Inject(appConfig.KEY)
+    private readonly appOptions: ConfigType<typeof appConfig>,
+  ) {}
+
+  private getUploadDirectoryPath(): string {
+    return this.appOptions.uploadDirectory;
+  }
+
+  private getDestinationFilePath(filename: string): string {
+    return join(
+      this.getUploadDirectoryPath(),
+      this.getSubUploadDirectoryPath(),
+      filename,
+    );
+  }
+
+  private getSubUploadDirectoryPath(): string {
+    const [year, month] = dayjs().format(this.DATE_FORMAT).split(' ');
+    return join(year, month);
+  }
+
+  public async writeFile(file: Express.Multer.File): Promise<StoredFile> {
+    try {
+      const uploadDirectoryPath = this.getUploadDirectoryPath();
+      const subDirectory = this.getSubUploadDirectoryPath();
+      const fileExtension = extension(file.mimetype);
+      const filename = `${randomUUID()}.${fileExtension}`;
+
+      const path = this.getDestinationFilePath(filename);
+
+      await ensureDir(join(uploadDirectoryPath, subDirectory));
+      await writeFile(path, file.buffer);
+
+      return {
+        fileExtension,
+        filename,
+        path,
+        subDirectory,
+      };
+    } catch (error) {
+      this.logger.error(`Error while saving file: ${error.message}`);
+      throw new Error(`Can't save file`);
+    }
+  }
+}
