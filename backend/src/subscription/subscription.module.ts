ebf6b3527c69ea91a33217e61fb1714987e501a0
Arina <arinakovalenco7@gmail.com>
2024-04-04 18:13:05 +0300

    Добавит SubscriptionModel

--- backend/src/subscription/subscription.module.ts
+++ backend/src/subscription/subscription.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { SubscriptionModel, SubscriptionSchema } from './subscription.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: SubscriptionModel.name, schema: SubscriptionSchema },
+    ]),
+  ],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService]
+  providers: [SubscriptionService],
 })
 export class SubscriptionModule {}
