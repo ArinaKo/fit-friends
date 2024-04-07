073c5aa7c8e578654fc0b3b5980625df8f35e987
Arina <arinakovalenco7@gmail.com>
2024-04-08 02:07:20 +0300

    Вынесет константы модуля mock

---
+++ backend/src/mock/mock.const.ts
@@ -0,0 +1,15 @@
+export const MAX_PRICE = 50000;
+export const DEFAULT_PASSWORD = '123456';
+
+export enum GeneratedDataAmount {
+  Workouts = 5,
+  Coaches = 5,
+  Comments = 5,
+  Orders = 3,
+  WorkoutRequests = 3,
+}
+
+export enum FilesAmount {
+  Avatars = 5,
+  Certificates = 6,
+}
