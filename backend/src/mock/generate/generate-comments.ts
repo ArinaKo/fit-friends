dbc598dc2266baa178be350f8074b0ad10e03eb9
Arina <arinakovalenco7@gmail.com>
2024-04-08 02:27:53 +0300

    Дополнит генерацию комментариев

--- backend/src/mock/generate/generate-comments.ts
+++ backend/src/mock/generate/generate-comments.ts
@@ -1,20 +1,33 @@
-import { generateDate, generateRandomValue, getRandomItem } from '@app/helpers';
+import { generateRandomValue, getRandomItem } from '@app/helpers';
 import { RatingValue } from 'src/shared/const';
 import { CommentsTexts } from './mock-data';
+import { GeneratedDataAmount } from '../mock.const';
+import { CommentEntity } from 'src/comment/comment.entity';
 
-const COMMENTS_NUMBER = 5;
-
-function generateComment(usersIds) {
+function generateComment(usersIds: string[]) {
   return {
     userId: getRandomItem(usersIds),
     rating: generateRandomValue(RatingValue.Min, RatingValue.Max),
     text: getRandomItem(CommentsTexts),
-    createdAt: generateDate(),
   };
 }
 
-export function generatesComments(workoutId: string, usersIds: string[]) {
-  return Array.from({ length: COMMENTS_NUMBER }).forEach(() =>
-    Object.assign(generateComment(usersIds), { workoutId }),
+function generatesCommentsForWorkout(
+  workoutId: string,
+  usersIds: string[],
+): CommentEntity[] {
+  return Array.from({ length: GeneratedDataAmount.Comments }).map(() =>
+    CommentEntity.fromObject(
+      Object.assign(generateComment(usersIds), { workoutId }),
+    ),
   );
+}
+
+export function generatesCommentsEntities(
+  workoutsIds: string[],
+  usersIds: string[],
+): CommentEntity[] {
+  return workoutsIds
+    .map((workoutId) => generatesCommentsForWorkout(workoutId, usersIds))
+    .flat();
 }
