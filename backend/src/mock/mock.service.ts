13d2a929dba805b3fa9144013230180f50a92d03
Arina <arinakovalenco7@gmail.com>
2024-04-08 18:34:08 +0300

    Добавит приватные методы генерации сущностей в MockService

--- backend/src/mock/mock.service.ts
+++ backend/src/mock/mock.service.ts
@@ -10,6 +10,20 @@ import { NotificationRepository } from 'src/notification/notification.repository
 import { SubscriberRepository } from 'src/subscriber/subscriber.repository';
 import { WorkoutService } from 'src/workout/workout.service';
 import { BalanceService } from 'src/balance/balance.service';
+import {
+  generateAvatarsEntities,
+  generateCertificatesEntities,
+  generateVideosEntities,
+  generateUsersEntities,
+  generateWorkoutsEntities,
+  generateCommentsEntities,
+  generateRequestsEntities,
+  generateOrdersEntities,
+  generateFriendsEntities,
+  generateNotificationsEntities,
+  generateSubscribersEntities,
+} from './generate';
+import { WorkoutEntity } from 'src/workout/workout.entity';
 
 @Injectable()
 export class MockService {
@@ -26,6 +40,97 @@ export class MockService {
     private readonly workoutService: WorkoutService,
     private readonly balanceService: BalanceService,
   ) {}
+
+  private async registerFilesInfo(): Promise<{
+    avatarsIds: string[];
+    certificatesIds: string[];
+    videosIds: string[];
+  }> {
+    const avatarsIds = (
+      await this.fileVaultRepository.saveMany(generateAvatarsEntities())
+    ).map((entity) => entity.id!);
+    const certificatesIds = (
+      await this.fileVaultRepository.saveMany(generateCertificatesEntities())
+    ).map((entity) => entity.id!);
+    const videosIds = (
+      await this.fileVaultRepository.saveMany(generateVideosEntities())
+    ).map((entity) => entity.id!);
+
+    return { avatarsIds, certificatesIds, videosIds };
+  }
+
+  private async registerUsers(
+    avatarsIds: string[],
+    certificatesIds: string[],
+  ): Promise<string[]> {
+    return (
+      await this.userRepository.saveMany(
+        await generateUsersEntities(avatarsIds, certificatesIds),
+      )
+    ).map((entity) => entity.id!);
+  }
+
+  private async registerWorkouts(
+    coachesIds: string[],
+    videosIds: string[],
+  ): Promise<WorkoutEntity[]> {
+    return this.workoutRepository.saveMany(
+      generateWorkoutsEntities(coachesIds, videosIds),
+    );
+  }
+
+  private async registerComments(
+    workoutsIds: string[],
+    usersIds: string[],
+  ): Promise<void> {
+    this.commentRepository.saveMany(
+      generateCommentsEntities(workoutsIds, usersIds),
+    );
+  }
+
+  private async registerRequests(
+    usersIds: string[],
+    allUsersIds: string[],
+  ): Promise<void> {
+    this.workoutRequestRepository.saveMany(
+      generateRequestsEntities(usersIds, allUsersIds),
+    );
+  }
+
+  private async registerOrders(
+    usersIds: string[],
+    workouts: WorkoutEntity[],
+  ): Promise<void> {
+    const orders = await this.orderRepository.saveMany(
+      generateOrdersEntities(usersIds, workouts),
+    );
+    orders.forEach(async (order) => {
+      await this.balanceService.increaseBalance(
+        order.userId,
+        order.workoutId,
+        order.count,
+      );
+    });
+  }
+
+  private async registerFriends(allUsersIds: string[]): Promise<void> {
+    this.friendsRepository.saveMany(generateFriendsEntities(allUsersIds));
+  }
+
+  private async registerSubscribers(
+    usersIds: string[],
+    coachesIds: string[],
+  ): Promise<void> {
+    this.subscriberRepository.saveMany(
+      generateSubscribersEntities(usersIds, coachesIds),
+    );
+  }
+
+  private async registerNotifications(allUsersIds: string[]): Promise<void> {
+    this.notificationRepository.saveMany(
+      generateNotificationsEntities(allUsersIds),
+    );
+  }
 
   public async generateMocks(): Promise<void> {}
 }
