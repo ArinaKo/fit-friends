a2bcc1aa79bf48f43bd42e057cc7f0184ccd396b
Arina <arinakovalenco7@gmail.com>
2024-04-15 23:40:54 +0300

    Подключит redux-thunk

---
+++ frontend/src/const/api.ts
@@ -0,0 +1,2 @@
+export const BACKEND_URL = 'http://localhost:3000';
+export const REQUEST_TIMEOUT = 5000;
--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -1,4 +1,15 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { rootReducer } from './root-reducer';
+import { createAPI } from '../services';
 
-export const store = configureStore({ reducer: rootReducer });
+export const api = createAPI();
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
