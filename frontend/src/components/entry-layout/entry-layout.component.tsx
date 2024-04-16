ba560d670342c64291e9898563381c6f417815e7
Arina <arinakovalenco7@gmail.com>
2024-04-16 21:10:59 +0300

    Реализует EntryLayout компонент

--- frontend/src/components/entry-layout/entry-layout.component.tsx
+++ frontend/src/components/entry-layout/entry-layout.component.tsx
@@ -1,14 +1,30 @@
-import { Fragment } from 'react';
 import { Outlet } from 'react-router-dom';
 
 function EntryLayout(): JSX.Element {
   return (
-    <Fragment>
-      <div>Not implemented - entry layout</div>
+    <div className="wrapper">
       <main>
+        <div className="background-logo">
+          <svg
+            className="background-logo__logo"
+            width={750}
+            height={284}
+            aria-hidden="true"
+          >
+            <use xlinkHref="#logo-big" />
+          </svg>
+          <svg
+            className="background-logo__icon"
+            width={343}
+            height={343}
+            aria-hidden="true"
+          >
+            <use xlinkHref="#icon-logotype" />
+          </svg>
+        </div>
         <Outlet />
       </main>
-    </Fragment>
+    </div>
   );
 }
 
