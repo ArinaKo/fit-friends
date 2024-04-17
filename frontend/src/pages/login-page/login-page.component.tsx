0369cfdb8bdd0fe0c8b743f90da23ff638790680
Arina <arinakovalenco7@gmail.com>
2024-04-17 16:15:27 +0300

    Подключит компонент LoginForm

--- frontend/src/pages/login-page/login-page.component.tsx
+++ frontend/src/pages/login-page/login-page.component.tsx
@@ -5,6 +5,7 @@ import {
   isAuthRequesting,
   isUserAuth,
   isUserCoach,
+  resetUserForm,
 } from '../../store';
 import { useEffect } from 'react';
 import { AppRoute } from '../../const';
@@ -26,6 +27,10 @@ function LoginPage(): JSX.Element {
     }
   }, [dispatch, isAuth]);
 
+  useEffect(() => {
+    dispatch(resetUserForm());
+  }, [dispatch]);
+
   return (
     <div className="popup-form popup-form--sign-in">
       <div className="popup-form__wrapper">
@@ -34,29 +39,7 @@ function LoginPage(): JSX.Element {
             <h1 className="popup-form__title">Вход</h1>
           </div>
           <div className="popup-form__form">
-            <form method="get">
-              <div className="sign-in">
-                <div className="custom-input sign-in__input">
-                  <label>
-                    <span className="custom-input__label">E-mail</span>
-                    <span className="custom-input__wrapper">
-                      <input type="email" name="email" />
-                    </span>
-                  </label>
-                </div>
-                <div className="custom-input sign-in__input">
-                  <label>
-                    <span className="custom-input__label">Пароль</span>
-                    <span className="custom-input__wrapper">
-                      <input type="password" name="password" />
-                    </span>
-                  </label>
-                </div>
-                <button className="btn sign-in__button" type="submit">
-                  Продолжить
-                </button>
-              </div>
-            </form>
+            <LoginForm />
           </div>
         </div>
       </div>
