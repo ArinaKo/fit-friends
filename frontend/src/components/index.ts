423173399d9716e0c6c4046f231ecd44a60e7293
Arina <arinakovalenco7@gmail.com>
2024-04-17 12:34:14 +0300

    Добавит компонент-спиннер

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -3,3 +3,4 @@ export { default as App } from './app/app.component';
 export { default as PrivateRoute } from './private-route/private-route.component';
 export { default as EntryLayout } from './entry-layout/entry-layout.component';
 export { default as Layout } from './layout/layout.component';
+export { default as UIBlocker } from './ui-blocker/ui-blocker.component';
---
+++ frontend/src/components/ui-blocker/ui-blocker.component.tsx
@@ -0,0 +1,11 @@
+import './ui-blocker.style.css';
+
+function UIBlocker(): JSX.Element {
+  return (
+    <div className="ui-blocker">
+      <span className="ui-blocker__text">Loading...</span>
+    </div>
+  );
+}
+
+export default UIBlocker;
