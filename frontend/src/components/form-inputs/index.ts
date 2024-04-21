a56660552be7fe477dc602862b409e88ec2a2898
Arina <arinakovalenco7@gmail.com>
2024-04-21 16:17:05 +0300

    Обновляет компонент RegisterForm

--- frontend/src/components/form-inputs/index.ts
+++ frontend/src/components/form-inputs/index.ts
@@ -1,2 +1,8 @@
 export { default as EmailInput } from './email-input/email-input.component';
 export { default as PasswordInput } from './password-input/password-input.component';
+export { default as NameInput } from './name-input/name-input.component';
+export { default as DateOfBirthInput } from './date-of-birth-input/day-of-birth-input.component';
+export { default as LocationInput } from './location-input/location-input.component';
+export { default as UserSexInput } from './user-sex-input/user-sex-input.component';
+export { default as RoleInput } from './role-input/role-input.component';
+export { default as AvatarInput } from './avatar-input/avatar-input.component';
--- frontend/src/components/register-form/register-form.component.tsx
+++ frontend/src/components/register-form/register-form.component.tsx
@@ -1,85 +1,48 @@
-import { ChangeEvent, FormEvent, useState } from 'react';
+import { FormEvent, useState } from 'react';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import {
-  getUserFormDateOfBirth,
-  getUserFormEmail,
-  getUserFormLocation,
-  getUserFormName,
-  getUserFormPassword,
-  getUserFormRole,
-  getUserFormSex,
   isUserFormDataSending,
-  setDateOfBirth,
-  setEmail,
-  setLocation,
-  setName,
-  setPassword,
-  setRole,
-  setSex,
+  isUserFormHaveErrors,
+  setRegisterRequiredFields,
 } from '../../store';
-import {
-  AppRoute,
-  MetroStation,
-  RoleInputLabel,
-  UserRole,
-  UserSex,
-} from '../../const';
+import { AppRoute } from '../../const';
 import { registerAction } from '../../store/api-actions';
-import lodash from 'lodash';
-import dayjs from 'dayjs';
 import { redirectToRoute } from '../../store/actions';
+import {
+  AvatarInput,
+  DateOfBirthInput,
+  EmailInput,
+  LocationInput,
+  NameInput,
+  PasswordInput,
+  RoleInput,
+  UserSexInput,
+} from '../form-inputs';
 
 function RegisterForm(): JSX.Element {
   const dispatch = useAppDispatch();
   const isSending = useAppSelector(isUserFormDataSending);
-  const email = useAppSelector(getUserFormEmail);
-  const password = useAppSelector(getUserFormPassword);
-  const name = useAppSelector(getUserFormName);
-  const dateOfBirth = useAppSelector(getUserFormDateOfBirth);
-  const userSex = useAppSelector(getUserFormSex);
-  const userRole = useAppSelector(getUserFormRole);
-  const location = useAppSelector(getUserFormLocation);
+  const isFormHaveError = useAppSelector(isUserFormHaveErrors);
 
-  const [isOpen, setIsOpen] = useState(false);
+  const [file, setFile] = useState<Blob | null>(null);
 
   const handleFormSubmit = (evt: FormEvent<HTMLFormElement>): void => {
     evt.preventDefault();
-    dispatch(
-      registerAction({
-        email,
-        password,
-        name,
-        sex: userSex,
-        role: userRole,
-        location: location,
-        dateOfBirth: dateOfBirth.length ? new Date(dateOfBirth) : undefined,
-        avatar: '6614adb311a9567c1b1a513e',
-      })
-    ).then((res) => {
-      if (res.meta.requestStatus === 'fulfilled') {
-        dispatch(redirectToRoute(AppRoute.Questionary));
-      }
-    });
+    dispatch(setRegisterRequiredFields());
+    if (!isFormHaveError && file) {
+      dispatch(registerAction(file)).then((res) => {
+        if (res.meta.requestStatus === 'fulfilled') {
+          dispatch(redirectToRoute(AppRoute.Questionary));
+        }
+      });
+    }
   };
 
   return (
     <form method="post" onSubmit={handleFormSubmit}>
       <div className="sign-up">
         <div className="sign-up__load-photo">
-          <div className="input-load-avatar">
-            <label>
-              <input
-                className="visually-hidden"
-                type="file"
-                accept="image/png, image/jpeg"
-              />
-              <span className="input-load-avatar__btn">
-                <svg width={20} height={20} aria-hidden="true">
-                  <use xlinkHref="#icon-import" />
-                </svg>
-              </span>
-            </label>
-          </div>
+          <AvatarInput setFile={setFile} />
           <div className="sign-up__description">
             <h2 className="sign-up__legend">Загрузите фото профиля</h2>
             <span className="sign-up__text">
@@ -88,171 +51,14 @@ function RegisterForm(): JSX.Element {
           </div>
         </div>
         <div className="sign-up__data">
-          <div className="custom-input">
-            <label>
-              <span className="custom-input__label">Имя</span>
-              <span className="custom-input__wrapper">
-                <input
-                  type="text"
-                  name="name"
-                  value={name}
-                  disabled={isSending}
-                  onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                    dispatch(setName(target.value));
-                  }}
-                />
-              </span>
-            </label>
-          </div>
-          <div className="custom-input">
-            <label>
-              <span className="custom-input__label">E-mail</span>
-              <span className="custom-input__wrapper">
-                <input
-                  type="email"
-                  name="email"
-                  value={email}
-                  disabled={isSending}
-                  onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                    dispatch(setEmail(target.value));
-                  }}
-                />
-              </span>
-            </label>
-          </div>
-          <div className="custom-input">
-            <label>
-              <span className="custom-input__label">Дата рождения</span>
-              <span className="custom-input__wrapper">
-                <input
-                  type="date"
-                  name="birthday"
-                  max="2099-12-31"
-                  value={
-                    dateOfBirth
-                      ? String(dayjs(dateOfBirth).format('YYYY-MM-DD'))
-                      : ''
-                  }
-                  disabled={isSending}
-                  onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                    dispatch(setDateOfBirth(target.value));
-                  }}
-                />
-              </span>
-            </label>
-          </div>
-          <div
-            className={`custom-select ${
-              location ? '' : 'custom-select--not-selected'
-            } ${isOpen ? 'is-open' : ''}`}
-          >
-            <span className="custom-select__label">Ваша локация</span>
-            {location ? (
-              <div className="custom-select__placeholder">
-                ст. м. {location}
-              </div>
-            ) : (
-              ''
-            )}
-            <button
-              className="custom-select__button"
-              type="button"
-              aria-label="Выберите одну из опций"
-              onClick={() => setIsOpen(!isOpen)}
-            >
-              <span className="custom-select__text" />
-              <span className="custom-select__icon">
-                <svg width={15} height={6} aria-hidden="true">
-                  <use xlinkHref="#arrow-down" />
-                </svg>
-              </span>
-            </button>
-            <ul className="custom-select__list" role="listbox">
-              {Object.values(MetroStation).map((station) => (
-                <li
-                  key={`metro-${station}`}
-                  onClick={() => {
-                    dispatch(setLocation(station));
-                    setIsOpen(false);
-                  }}
-                  className="custom-select__item"
-                >
-                  {station}
-                </li>
-              ))}
-            </ul>
-          </div>
-          <div className="custom-input">
-            <label>
-              <span className="custom-input__label">Пароль</span>
-              <span className="custom-input__wrapper">
-                <input
-                  type="password"
-                  name="password"
-                  autoComplete="off"
-                  value={password}
-                  disabled={isSending}
-                  onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                    dispatch(setPassword(target.value));
-                  }}
-                />
-              </span>
-            </label>
-          </div>
-          <div className="sign-up__radio">
-            <span className="sign-up__label">Пол</span>
-            <div className="custom-toggle-radio custom-toggle-radio--big">
-              {Object.values(UserSex).map((sex) => (
-                <div className="custom-toggle-radio__block" key={`sex-${sex}`}>
-                  <label>
-                    <input
-                      type="radio"
-                      name="sex"
-                      value={sex}
-                      disabled={isSending}
-                      checked={sex === userSex}
-                      onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                        dispatch(setSex(target.value as UserSex));
-                      }}
-                    />
-                    <span className="custom-toggle-radio__icon" />
-                    <span className="custom-toggle-radio__label">
-                      {lodash.capitalize(sex)}
-                    </span>
-                  </label>
-                </div>
-              ))}
-            </div>
-          </div>
+          <NameInput />
+          <EmailInput />
+          <DateOfBirthInput />
+          <LocationInput />
+          <PasswordInput />
+          <UserSexInput />
         </div>
-        <div className="sign-up__role">
-          <h2 className="sign-up__legend">Выберите роль</h2>
-          <div className="role-selector sign-up__role-selector">
-            {Object.values(UserRole).map((role) => (
-              <div className="role-btn" key={`role-${role}`}>
-                <label>
-                  <input
-                    className="visually-hidden"
-                    type="radio"
-                    name="role"
-                    value={role}
-                    disabled={isSending}
-                    checked={role === userRole}
-                    onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
-                      dispatch(setRole(target.value as UserRole));
-                    }}
-                  />
-                  <span className="role-btn__icon">
-                    <svg width={12} height={13} aria-hidden="true">
-                      <use xlinkHref="#icon-cup" />
-                    </svg>
-                  </span>
-                  <span className="role-btn__btn">{RoleInputLabel[role]}</span>
-                </label>
-              </div>
-            ))}
-          </div>
-        </div>
+        <RoleInput />
         <div className="sign-up__checkbox">
           <label>
             <input
