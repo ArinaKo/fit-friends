0064eeda1e3636da80d3bb7fcb3632692bc4ab62
Arina <arinakovalenco7@gmail.com>
2024-05-01 23:04:32 +0300

    Дополнит константы

--- frontend/src/const/workout.ts
+++ frontend/src/const/workout.ts
@@ -15,3 +15,23 @@ export enum WorkoutDuration {
   Long = '50-80 мин',
   Extra = '80-100 мин',
 }
+
+export enum WorkoutSexFor {
+  Male = 'мужчинам',
+  Female = 'женщинам',
+  All = 'всем',
+}
+
+export enum WorkoutTitleLength {
+  Min = 1,
+  Max = 15,
+}
+
+export enum WorkoutDescriptionLength {
+  Min = 10,
+  Max = 140,
+}
+
+export enum PriceValue {
+  Min = 0,
+}
