1f928a01b7d6ef553d59464c13e32b75802141cd
Arina <arinakovalenco7@gmail.com>
2024-05-01 23:10:57 +0300

    Добавит компонент RadioInput

---
+++ frontend/src/components/form-inputs/radio-input/radio-input.component.tsx
@@ -0,0 +1,55 @@
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { RadioInputType, RadioInputTypeDiffs } from './radio-input';
+import lodash from 'lodash';
+import cn from 'classnames';
+
+type RadioInputProps = {
+  type: RadioInputType;
+  styleClass?: string;
+};
+
+function RadioInput({ type, styleClass }: RadioInputProps): JSX.Element {
+  const {
+    valueSelector,
+    setValue,
+    optionsArray,
+    formStatusSelector,
+    fieldName,
+    styleMode,
+  } = RadioInputTypeDiffs[type];
+  const dispatch = useAppDispatch();
+  const value = useAppSelector(valueSelector);
+  const isDisabled = useAppSelector(formStatusSelector);
+
+  return (
+    <div
+      className={cn('custom-toggle-radio', {
+        [`${styleClass ?? ''}__radio`]: styleClass,
+        styleMode,
+      })}
+    >
+      {Object.values(optionsArray).map((option) => (
+        <div className="custom-toggle-radio__block" key={`option-${option}`}>
+          <label>
+            <input
+              type="radio"
+              name={fieldName}
+              value={value}
+              disabled={isDisabled}
+              checked={value === option}
+              onChange={() => {
+                dispatch(setValue(option));
+              }}
+            />
+            <span className="custom-toggle-radio__icon" />
+            <span className="custom-toggle-radio__label">
+              {lodash.capitalize(option)}
+            </span>
+          </label>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default RadioInput;
---
+++ frontend/src/components/form-inputs/radio-input/radio-input.ts
@@ -0,0 +1,73 @@
+import { ActionCreatorWithPayload } from '@reduxjs/toolkit';
+import {
+  getUserFormLevel,
+  getUserFormSex,
+  getUserFormTimeForWorkout,
+  getWorkoutFormUserSex,
+  isUserFormDataSending,
+  isWorkoutFormDataSending,
+  setLevel,
+  setSex,
+  setTimeForWorkout,
+  setUserSexFor,
+} from '../../../store';
+import { State } from '../../../types';
+import { UserLevel, UserSex, WorkoutDuration, WorkoutSexFor } from '../../../const';
+
+export enum RadioInputType {
+  Level = 'level',
+  Sex = 'sex',
+  TimeForWorkout = 'time-for-workout',
+  UserSexFor = 'user-sex-for',
+}
+
+enum StyleMode {
+  Big = 'custom-toggle-radio--big',
+}
+
+type RadioInputTypeDiff = {
+  valueSelector: (state: State) => string | undefined;
+  setValue: ActionCreatorWithPayload<string>;
+  optionsArray: string[];
+  formStatusSelector: (state: State) => boolean;
+  fieldName: string;
+  styleMode?: StyleMode;
+};
+
+type RadioInputTypeDiffs = {
+  [type: string]: RadioInputTypeDiff;
+};
+
+export const RadioInputTypeDiffs: RadioInputTypeDiffs = {
+  [RadioInputType.Level]: {
+    valueSelector: getUserFormLevel,
+    setValue: setLevel,
+    optionsArray: Object.values(UserLevel),
+    formStatusSelector: isUserFormDataSending,
+    fieldName: 'level',
+    styleMode: StyleMode.Big,
+  },
+  [RadioInputType.Sex]: {
+    valueSelector: getUserFormSex,
+    setValue: setSex,
+    optionsArray: Object.values(UserSex),
+    formStatusSelector: isUserFormDataSending,
+    fieldName: 'sex',
+    styleMode: StyleMode.Big,
+  },
+  [RadioInputType.TimeForWorkout]: {
+    valueSelector: getUserFormTimeForWorkout,
+    setValue: setTimeForWorkout,
+    optionsArray: Object.values(WorkoutDuration),
+    formStatusSelector: isUserFormDataSending,
+    fieldName: 'timeForWorkout',
+    styleMode: StyleMode.Big,
+  },
+  [RadioInputType.UserSexFor]: {
+    valueSelector: getWorkoutFormUserSex,
+    setValue: setUserSexFor,
+    optionsArray: Object.values(WorkoutSexFor),
+    formStatusSelector: isWorkoutFormDataSending,
+    fieldName: 'userSexFor',
+  },
+};
