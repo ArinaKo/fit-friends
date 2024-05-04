d1a87454cc683f37ef15c9c9c03f3911050eb714
Arina <arinakovalenco7@gmail.com>
2024-05-04 19:19:59 +0300

    Добавит компонент RangeInput

---
+++ frontend/src/components/filter-inputs/range-input/range-input.component.tsx
@@ -0,0 +1,158 @@
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { useAppDispatch, useAppSelector } from '../../../hooks';
+import { RangeInputType, RangeInputTypeDiffs } from './range-input';
+import './range-input.css';
+
+type RangeInputProps = {
+  type: RangeInputType;
+};
+
+function RangeInput({ type }: RangeInputProps): JSX.Element {
+  const {
+    name,
+    withFields,
+    minValueSelector,
+    maxValueSelector,
+    minFilterSelector,
+    maxFilterSelector,
+    setMinFilter,
+    setMaxFilter,
+  } = RangeInputTypeDiffs[type];
+  const dispatch = useAppDispatch();
+  const minValue = useAppSelector(minValueSelector);
+  const maxValue = useAppSelector(maxValueSelector);
+  const minFilter = useAppSelector(minFilterSelector);
+  const maxFilter = useAppSelector(maxFilterSelector);
+
+  const range = useRef<HTMLDivElement>(null);
+  const [minInput, setMinInput] = useState<number>(minValue);
+  const [maxInput, setMaxInput] = useState<number>(maxValue);
+
+  const getPercent = useCallback(
+    (value: number) =>
+      Math.round(((value - minValue) / (maxValue - minValue)) * 100),
+    [minValue, maxValue],
+  );
+
+  useEffect(() => {
+    setMinInput(minFilter ?? minValue);
+    setMaxInput(maxFilter ?? maxValue);
+  }, [minValue, maxValue, minFilter, maxFilter]);
+
+  useEffect(() => {
+    const minPercent = getPercent(minFilter ?? minValue);
+    const maxPercent = getPercent(maxFilter ?? maxValue);
+
+    if (range.current) {
+      range.current.style.left = `${minPercent}%`;
+      range.current.style.width = `${maxPercent - minPercent}%`;
+    }
+  }, [getPercent, minFilter, maxFilter, minValue, maxValue]);
+
+  return (
+    <>
+      {withFields ? (
+        <div className={`filter-${name}`}>
+          <div
+            className={`filter-${name}__input-text filter-${name}__input-text--min`}
+          >
+            <input
+              type="number"
+              id={`${name}-min`}
+              name={`${name}-min`}
+              min={0}
+              max={maxValue}
+              autoComplete="off"
+              step={1}
+              value={minInput}
+              onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
+                const value = Number(target.value);
+                setMinInput(value);
+                if (value >= minValue && value <= (maxFilter ?? maxValue)) {
+                  dispatch(setMinFilter(value));
+                }
+              }}
+            />
+            <label htmlFor={`${name}-min`}>от</label>
+          </div>
+          <div
+            className={`filter-${name}__input-text filter-${name}__input-text--max`}
+          >
+            <input
+              type="number"
+              id={`${name}-max`}
+              name={`${name}-max`}
+              min={0}
+              max={maxValue}
+              autoComplete="off"
+              step={1}
+              value={maxInput}
+              onChange={({ target }: ChangeEvent<HTMLInputElement>) => {
+                const value = Number(target.value);
+                setMaxInput(value);
+                if (value <= maxValue && value >= (minFilter ?? minValue)) {
+                  dispatch(setMaxFilter(value));
+                }
+              }}
+            />
+            <label htmlFor={`${name}-max`}>до</label>
+          </div>
+        </div>
+      ) : undefined}
+      <div className="filter-range">
+        <div className="filter-range__scale">
+          <div ref={range} className="filter-range__bar">
+            <span className="visually-hidden">Полоса прокрутки</span>
+          </div>
+        </div>
+        <div className="filter-range__control">
+          <input
+            type="range"
+            min={minValue}
+            max={maxValue}
+            value={minFilter ?? minValue}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              const value = Math.min(
+                Number(event.target.value),
+                maxFilter ?? maxValue - 1,
+              );
+              dispatch(setMinFilter(value));
+            }}
+            className="thumb thumb--left"
+            style={
+              minFilter && minFilter > maxValue - 10
+                ? { zIndex: '5' }
+                : undefined
+            }
+          />
+          <input
+            type="range"
+            min={minValue}
+            max={maxValue}
+            value={maxFilter ?? maxValue}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              const value = Math.max(
+                Number(event.target.value),
+                minFilter ?? minValue + 1,
+              );
+              dispatch(setMaxFilter(value));
+            }}
+            className="thumb thumb--right"
+          />
+          {withFields ? undefined : (
+            <>
+              <span className="filter-range__control--left">
+                {minFilter ?? minValue}
+              </span>
+              <span className="filter-range__control--right">
+                {maxFilter ?? maxValue}
+              </span>
+            </>
+          )}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default RangeInput;
---
+++ frontend/src/components/filter-inputs/range-input/range-input.ts
@@ -0,0 +1,72 @@
+import {
+  getWorkoutsFilterMaxCalories,
+  getWorkoutsFilterMaxPrice,
+  getWorkoutsFilterMaxRating,
+  getWorkoutsFilterMinCalories,
+  getWorkoutsFilterMinPrice,
+  getWorkoutsFilterMinRating,
+  getWorkoutsMaxCalories,
+  getWorkoutsMaxPrice,
+  getWorkoutsMaxRating,
+  getWorkoutsMinCalories,
+  getWorkoutsMinPrice,
+  getWorkoutsMinRating,
+  setWorkoutsCaloriesFilter,
+  setWorkoutsPriceFilter,
+  setWorkoutsRatingFilter,
+} from '../../../store';
+import { State } from '../../../types';
+
+export enum RangeInputType {
+  WorkoutPrice = 'workout-price',
+  WorkoutCalories = 'workout-calories',
+  WorkoutRating = 'workout-rating',
+}
+
+type RangeInputTypeDiff = {
+  name: string;
+  withFields: boolean;
+  minValueSelector: (state: State) => number;
+  maxValueSelector: (state: State) => number;
+  minFilterSelector: (state: State) => number | undefined;
+  maxFilterSelector: (state: State) => number | undefined;
+  setMinFilter: (value: number) => { payload: [string, number]; type: string };
+  setMaxFilter: (value: number) => { payload: [string, number]; type: string };
+};
+
+type RangeInputTypeDiffs = {
+  [type: string]: RangeInputTypeDiff;
+};
+
+export const RangeInputTypeDiffs: RangeInputTypeDiffs = {
+  [RangeInputType.WorkoutPrice]: {
+    name: 'price',
+    withFields: true,
+    minValueSelector: getWorkoutsMinPrice,
+    maxValueSelector: getWorkoutsMaxPrice,
+    minFilterSelector: getWorkoutsFilterMinPrice,
+    maxFilterSelector: getWorkoutsFilterMaxPrice,
+    setMinFilter: (value: number) => setWorkoutsPriceFilter(['min', value]),
+    setMaxFilter: (value: number) => setWorkoutsPriceFilter(['max', value]),
+  },
+  [RangeInputType.WorkoutCalories]: {
+    name: 'calories',
+    withFields: true,
+    minValueSelector: getWorkoutsMinCalories,
+    maxValueSelector: getWorkoutsMaxCalories,
+    minFilterSelector: getWorkoutsFilterMinCalories,
+    maxFilterSelector: getWorkoutsFilterMaxCalories,
+    setMinFilter: (value: number) => setWorkoutsCaloriesFilter(['min', value]),
+    setMaxFilter: (value: number) => setWorkoutsCaloriesFilter(['max', value]),
+  },
+  [RangeInputType.WorkoutRating]: {
+    name: 'rating',
+    withFields: false,
+    minValueSelector: getWorkoutsMinRating,
+    maxValueSelector: getWorkoutsMaxRating,
+    minFilterSelector: getWorkoutsFilterMinRating,
+    maxFilterSelector: getWorkoutsFilterMaxRating,
+    setMinFilter: (value: number) => setWorkoutsRatingFilter(['min', value]),
+    setMaxFilter: (value: number) => setWorkoutsRatingFilter(['max', value]),
+  },
+};
