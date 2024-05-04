9c353912e7cd487edafc84a01b0d3ddc594c8b58
Arina <arinakovalenco7@gmail.com>
2024-05-04 23:14:45 +0300

    Добавит компонент OrdersSorting

---
+++ frontend/src/components/orders-sorting/orders-sorting.component.tsx
@@ -0,0 +1,60 @@
+import { OrdersSortType } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import {
+  getOrdersListSortType,
+  isOrdersListSortDown,
+  setOrdersSorting,
+} from '../../store';
+import cn from 'classnames';
+
+function OrdersSorting(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const sortType = useAppSelector(getOrdersListSortType);
+  const isSortDown = useAppSelector(isOrdersListSortDown);
+
+  return (
+    <div className="sort-for">
+      <p>Сортировать по:</p>
+      <div className="sort-for__btn-container">
+        <button
+          className={cn('btn-filter-sort', {
+            'btn-filter-sort--active': sortType === OrdersSortType.Sum,
+          })}
+          type="button"
+          onClick={() => dispatch(setOrdersSorting(OrdersSortType.Sum))}
+        >
+          <span>Сумме</span>
+          <svg width={16} height={10} aria-hidden="true">
+            <use
+              xlinkHref={
+                !isSortDown && sortType === OrdersSortType.Sum
+                  ? '#icon-sort-down'
+                  : '#icon-sort-up'
+              }
+            />
+          </svg>
+        </button>
+        <button
+          className={cn('btn-filter-sort', {
+            'btn-filter-sort--active': sortType === OrdersSortType.Count,
+          })}
+          type="button"
+          onClick={() => dispatch(setOrdersSorting(OrdersSortType.Count))}
+        >
+          <span>Количеству</span>
+          <svg width={16} height={10} aria-hidden="true">
+            <use
+              xlinkHref={
+                !isSortDown && sortType === OrdersSortType.Count
+                  ? '#icon-sort-down'
+                  : '#icon-sort-up'
+              }
+            />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default OrdersSorting;
