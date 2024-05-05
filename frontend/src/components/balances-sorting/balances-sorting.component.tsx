4a2e54d9dfe5ee0178034f84b9b2962440a7e7ba
Arina <arinakovalenco7@gmail.com>
2024-05-05 17:00:05 +0300

    Добавит компонент BalancesSorting

---
+++ frontend/src/components/balances-sorting/balances-sorting.component.tsx
@@ -0,0 +1,40 @@
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import {
+  isOnlyActiveBalances,
+  resetCatalogPage,
+  setBalancesSorting,
+} from '../../store';
+
+function BalancesSorting(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const isOnlyActive = useAppSelector(isOnlyActiveBalances);
+
+  return (
+    <div className="my-purchases__controls">
+      <div
+        className="custom-toggle custom-toggle--switch custom-toggle--switch-right my-purchases__switch"
+        data-validate-type="checkbox"
+      >
+        <label>
+          <input
+            type="checkbox"
+            name="active-balances"
+            checked={isOnlyActive}
+            onChange={() => {
+              dispatch(resetCatalogPage());
+              dispatch(setBalancesSorting(!isOnlyActive));
+            }}
+          />
+          <span className="custom-toggle__icon">
+            <svg width={9} height={6} aria-hidden="true">
+              <use xlinkHref="#arrow-check" />
+            </svg>
+          </span>
+          <span className="custom-toggle__label">Только активные</span>
+        </label>
+      </div>
+    </div>
+  );
+}
+
+export default BalancesSorting;
