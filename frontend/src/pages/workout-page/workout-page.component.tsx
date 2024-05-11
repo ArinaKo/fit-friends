5f61c42b12f00fca44385499f4a7106e1d552115
Arina <arinakovalenco7@gmail.com>
2024-05-11 14:59:43 +0300

    Дополнит компонент WorkoutPage

--- frontend/src/pages/workout-page/workout-page.component.tsx
+++ frontend/src/pages/workout-page/workout-page.component.tsx
@@ -1,4 +1,45 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { AppRoute } from '../../const';
+import {
+  getWorkoutAction,
+  getWorkoutBalanceStatusAction,
+  getWorkoutId,
+  isUserHaveAccessToWorkout,
+  isWorkoutInfoLoading,
+} from '../../store';
+import { useEffect } from 'react';
+import { UIBlocker, WorkoutInfo } from '../../components';
+
 function WorkoutPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { workoutId } = useParams();
+  const isUserHaveAccess = useAppSelector(isUserHaveAccessToWorkout);
+  const currentWorkoutId = useAppSelector(getWorkoutId);
+  const isDataLoading = useAppSelector(isWorkoutInfoLoading);
+
+  useEffect(() => {
+    if (currentWorkoutId !== workoutId && !isDataLoading) {
+      dispatch(getWorkoutAction(workoutId as string));
+      dispatch(getWorkoutBalanceStatusAction(workoutId as string));
+    }
+    if (!isUserHaveAccess && currentWorkoutId === workoutId) {
+      navigate(AppRoute.Account);
+    }
+  }, [
+    navigate,
+    dispatch,
+    workoutId,
+    currentWorkoutId,
+    isDataLoading,
+    isUserHaveAccess,
+  ]);
+
+  if (isDataLoading) {
+    return <UIBlocker />;
+  }
+
   return (
     <section className="inner-page">
       <div className="container">
@@ -8,6 +49,7 @@ function WorkoutPage(): JSX.Element {
             <button
               className="btn-flat btn-flat--underlined reviews-side-bar__back"
               type="button"
+              onClick={() => navigate(AppRoute.Workouts)}
             >
               <svg width={14} height={10} aria-hidden="true">
                 <use xlinkHref="#arrow-left" />
@@ -219,172 +261,7 @@ function WorkoutPage(): JSX.Element {
               Оставить отзыв
             </button>
           </aside>
-          <div className="training-card">
-            <div className="training-info">
-              <h2 className="visually-hidden">Информация о тренировке</h2>
-              <div className="training-info__header">
-                <div className="training-info__coach">
-                  <div className="training-info__photo">
-                    <picture>
-                      <source
-                        type="image/webp"
-                        srcSet="img/content/avatars/coaches//photo-1.webp, img/content/avatars/coaches//photo-1@2x.webp 2x"
-                      />
-                      <img
-                        src="img/content/avatars/coaches//photo-1.png"
-                        srcSet="img/content/avatars/coaches//photo-1@2x.png 2x"
-                        width={64}
-                        height={64}
-                        alt="Изображение тренера"
-                      />
-                    </picture>
-                  </div>
-                  <div className="training-info__coach-info">
-                    <span className="training-info__label">Тренер</span>
-                    <span className="training-info__name">Валерия</span>
-                  </div>
-                </div>
-              </div>
-              <div className="training-info__main-content">
-                <form action="#" method="get">
-                  <div className="training-info__form-wrapper">
-                    <div className="training-info__info-wrapper">
-                      <div className="training-info__input training-info__input--training">
-                        <label>
-                          <span className="training-info__label">
-                            Название тренировки
-                          </span>
-                          <input
-                            type="text"
-                            name="training"
-                            defaultValue="energy"
-                            disabled
-                          />
-                        </label>
-                        <div className="training-info__error">
-                          Обязательное поле
-                        </div>
-                      </div>
-                      <div className="training-info__textarea">
-                        <label>
-                          <span className="training-info__label">
-                            Описание тренировки
-                          </span>
-                          <textarea
-                            name="description"
-                            disabled
-                            defaultValue={
-                              'Упражнения укрепляют мышечный корсет, делают суставы более гибкими, улучшают осанку и координацию.'
-                            }
-                          />
-                        </label>
-                      </div>
-                    </div>
-                    <div className="training-info__rating-wrapper">
-                      <div className="training-info__input training-info__input--rating">
-                        <label>
-                          <span className="training-info__label">Рейтинг</span>
-                          <span className="training-info__rating-icon">
-                            <svg width={18} height={18} aria-hidden="true">
-                              <use xlinkHref="#icon-star" />
-                            </svg>
-                          </span>
-                          <input
-                            type="number"
-                            name="rating"
-                            defaultValue={4}
-                            disabled
-                          />
-                        </label>
-                      </div>
-                      <ul className="training-info__list">
-                        <li className="training-info__item">
-                          <div className="hashtag hashtag--white">
-                            <span>#пилатес</span>
-                          </div>
-                        </li>
-                        <li className="training-info__item">
-                          <div className="hashtag hashtag--white">
-                            <span>#для_всех</span>
-                          </div>
-                        </li>
-                        <li className="training-info__item">
-                          <div className="hashtag hashtag--white">
-                            <span>#320ккал</span>
-                          </div>
-                        </li>
-                        <li className="training-info__item">
-                          <div className="hashtag hashtag--white">
-                            <span>#30_50минут</span>
-                          </div>
-                        </li>
-                      </ul>
-                    </div>
-                    <div className="training-info__price-wrapper">
-                      <div className="training-info__input training-info__input--price">
-                        <label>
-                          <span className="training-info__label">
-                            Стоимость
-                          </span>
-                          <input
-                            type="text"
-                            name="price"
-                            defaultValue="800 ₽"
-                            disabled
-                          />
-                        </label>
-                        <div className="training-info__error">
-                          Введите число
-                        </div>
-                      </div>
-                      <button className="btn training-info__buy" type="button">
-                        Купить
-                      </button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-            <div className="training-video">
-              <h2 className="training-video__title">Видео</h2>
-              <div className="training-video__video">
-                <div className="training-video__thumbnail">
-                  <picture>
-                    <source
-                      type="image/webp"
-                      srcSet="img/content/training-video/video-thumbnail.webp, img/content/training-video/video-thumbnail@2x.webp 2x"
-                    />
-                    <img
-                      src="img/content/training-video/video-thumbnail.png"
-                      srcSet="img/content/training-video/video-thumbnail@2x.png 2x"
-                      width={922}
-                      height={566}
-                      alt="Обложка видео"
-                    />
-                  </picture>
-                </div>
-                <button className="training-video__play-button btn-reset">
-                  <svg width={18} height={30} aria-hidden="true">
-                    <use xlinkHref="#icon-arrow" />
-                  </svg>
-                </button>
-              </div>
-              <div className="training-video__buttons-wrapper">
-                <button
-                  className="btn training-video__button training-video__button--start"
-                  type="button"
-                >
-                  Приступить
-                </button>
-                <button
-                  className="btn training-video__button training-video__button--stop"
-                  type="button"
-                >
-                  Закончить
-                </button>
-              </div>
-            </div>
-          </div>
+          <WorkoutInfo />
         </div>
       </div>
     </section>
