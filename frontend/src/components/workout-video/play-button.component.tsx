fac9c14a21fe04c6397d61aa7708ce81b0940c8f
Arina <arinakovalenco7@gmail.com>
2024-05-11 14:55:57 +0300

    Добавит компонент PlayButton

---
+++ frontend/src/components/workout-video/play-button.component.tsx
@@ -0,0 +1,28 @@
+import { useAppSelector } from '../../hooks';
+import { isCurrentWorkoutActive, isUserCoach } from '../../store';
+import cn from 'classnames';
+
+type PlayButtonProps = {
+  onClick: () => void;
+};
+
+function PlayButton({ onClick }: PlayButtonProps): JSX.Element {
+  const isCoach = useAppSelector(isUserCoach);
+  const isWorkoutActive = useAppSelector(isCurrentWorkoutActive);
+
+  return (
+    <button
+      className={cn('training-video__play-button btn-reset', {
+        'is-disabled': !isWorkoutActive && !isCoach,
+      })}
+      onClick={onClick}
+      disabled={!isWorkoutActive && !isCoach}
+    >
+      <svg width={18} height={30} aria-hidden="true">
+        <use xlinkHref="#icon-arrow" />
+      </svg>
+    </button>
+  );
+}
+
+export default PlayButton;
