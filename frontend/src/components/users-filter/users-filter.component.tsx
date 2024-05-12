16e24901175c372e9a0a7ed73e439ccd78208fca
Arina <arinakovalenco7@gmail.com>
2024-05-12 15:56:07 +0300

    Добавит компонент UsersFilter

---
+++ frontend/src/components/users-filter/users-filter.component.tsx
@@ -0,0 +1,41 @@
+import { CheckboxInput, CheckboxInputType } from '../filter-inputs';
+import { RadioInput, RadioInputType } from '../form-inputs';
+import { SortingInputType } from '../sorting-input/sorting-input';
+import SortingInput from '../sorting-input/sorting-input.component';
+
+const styleClass = 'user-catalog-form';
+
+function UsersFilter(): JSX.Element {
+  return (
+    <form className="user-catalog-form__form">
+      <div className="user-catalog-form__block user-catalog-form__block--location">
+        <h4 className="user-catalog-form__block-title">
+          Локация, станция метро
+        </h4>
+        <CheckboxInput
+          type={CheckboxInputType.UserLocation}
+          styleClass={styleClass}
+        />
+      </div>
+      <div className="user-catalog-form__block user-catalog-form__block--spezialization">
+        <h4 className="user-catalog-form__block-title">Специализация</h4>
+        <CheckboxInput
+          type={CheckboxInputType.UserWorkoutTypes}
+          styleClass={styleClass}
+        />
+      </div>
+      <div className="user-catalog-form__block user-catalog-form__block--level">
+        <h4 className="user-catalog-form__block-title">Ваш уровень</h4>
+        <RadioInput type={RadioInputType.UserLevelFilter} />
+      </div>
+      <div className="user-catalog-form__block">
+        <h3 className="user-catalog-form__title user-catalog-form__title--sort">
+          Сортировка
+        </h3>
+        <SortingInput type={SortingInputType.Users} />
+      </div>
+    </form>
+  );
+}
+
+export default UsersFilter;
