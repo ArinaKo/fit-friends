b19686bb7c9315166f4a6cf33effe68a4c6e4eda
Arina <arinakovalenco7@gmail.com>
2024-05-13 12:50:52 +0300

    Обновит генерацию уведомлений (backend)

--- backend/src/mock/generate/generate-notifications.ts
+++ backend/src/mock/generate/generate-notifications.ts
@@ -1,22 +1,29 @@
-import { getRandomItem } from '@app/helpers';
+import { generateRandomValue, getRandomItem } from '@app/helpers';
 import { FemaleNames, MaleNames } from './mock-data';
 import { GeneratedDataAmount } from '../mock.const';
 import { NotificationEntity } from 'src/notification/notification.entity';
 import { NotificationText } from 'src/notification/notification.const';
 
-function generateNotificationEntity(usersIds: string[]): NotificationEntity {
-  return NotificationEntity.fromObject({
-    userId: getRandomItem(usersIds),
+function generateNotification(userId: string) {
+  return {
+    userId,
     text: NotificationText.getNewFriendMessage(
       getRandomItem(FemaleNames.concat(MaleNames)),
     ),
-  });
+  };
+}
+
+function generateNotificationForUser(userId: string): NotificationEntity[] {
+  const amount = generateRandomValue(0, GeneratedDataAmount.Notifications);
+  return Array.from({ length: amount }).map(() =>
+    NotificationEntity.fromObject(generateNotification(userId)),
+  );
 }
 
 export function generateNotificationsEntities(
   allUsersIds: string[],
 ): NotificationEntity[] {
-  return Array.from({ length: GeneratedDataAmount.Notifications }).map(() =>
-    generateNotificationEntity(allUsersIds),
-  );
+  return allUsersIds
+    .map((userId) => generateNotificationForUser(userId))
+    .flat();
 }
