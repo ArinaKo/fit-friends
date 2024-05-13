a7552a9f26a9bec2f5dc990f35f66a28df8a2516
Arina <arinakovalenco7@gmail.com>
2024-05-13 20:09:31 +0300

    Дополнит компонент UserPage

--- frontend/src/pages/user-page/user-page.component.tsx
+++ frontend/src/pages/user-page/user-page.component.tsx
@@ -1,9 +1,44 @@
+import { useNavigate, useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { useEffect } from 'react';
+import { UIBlocker, UserInfo, UserInfoWorkouts } from '../../components';
+import {
+  getUserAction,
+  getUserId,
+  isUserLoading,
+  isUserRoleCoach,
+  setActiveRoute,
+} from '../../store';
+import { AppRoute } from '../../const';
+
 function UserPage(): JSX.Element {
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+  const { userId } = useParams();
+  const currentUserId = useAppSelector(getUserId);
+  const isUserCoach = useAppSelector(isUserRoleCoach);
+  const isDataLoading = useAppSelector(isUserLoading);
+
+  useEffect(() => {
+    if (currentUserId !== userId && !isDataLoading) {
+      dispatch(getUserAction(userId as string));
+    }
+    dispatch(setActiveRoute());
+  }, [dispatch, userId, currentUserId, isDataLoading]);
+
+  if (isDataLoading) {
+    return <UIBlocker />;
+  }
+
   return (
     <div className="inner-page inner-page--no-sidebar">
       <div className="container">
         <div className="inner-page__wrapper">
-          <button className="btn-flat inner-page__back" type="button">
+          <button
+            className="btn-flat inner-page__back"
+            type="button"
+            onClick={() => navigate(AppRoute.Users)}
+          >
             <svg width={14} height={10} aria-hidden="true">
               <use xlinkHref="#arrow-left" />
             </svg>
@@ -12,90 +47,14 @@ function UserPage(): JSX.Element {
           <div className="inner-page__content">
             <section className="user-card">
               <h1 className="visually-hidden">Карточка пользователя</h1>
-              <div className="user-card__wrapper">
-                <div className="user-card__content">
-                  <div className="user-card__head">
-                    <h2 className="user-card__title">Катерина</h2>
-                  </div>
-                  <div className="user-card__label">
-                    <a href="">
-                      <svg
-                        className="user-card-coach__icon-location"
-                        width={12}
-                        height={14}
-                        aria-hidden="true"
-                      >
-                        <use xlinkHref="#icon-location" />
-                      </svg>
-                      <span>Невский проспект</span>
-                    </a>
-                  </div>
-                  <div className="user-card__status">
-                    <span>Готов к тренировке</span>
-                  </div>
-                  <div className="user-card__text">
-                    <p>
-                      Привет! Я&nbsp;Катерина и&nbsp;мне 27 лет. Обожаю спорт
-                      и&nbsp;все, что с&nbsp;ним связанно. Регулярно хожу
-                      на&nbsp;тренировки по&nbsp;кроссфиту, также занимаюсь
-                      йогой, рястяжкой и&nbsp;пилатесом.
-                    </p>
-                    <p>
-                      Занимаюсь как с&nbsp;тренером индивидуально, так
-                      и&nbsp;на&nbsp;групповых занятиях. Люблю соревнования
-                      и&nbsp;челленджи, так что присоединяйтесь, давайте
-                      объединяться и&nbsp;заниматься вместе!)
-                    </p>
-                  </div>
-                  <ul className="user-card__hashtag-list">
-                    <li className="user-card__hashtag-item">
-                      <div className="hashtag">
-                        <span>#йога</span>
-                      </div>
-                    </li>
-                    <li className="user-card__hashtag-item">
-                      <div className="hashtag">
-                        <span>#кроссфит</span>
-                      </div>
-                    </li>
-                    <li className="user-card__hashtag-item">
-                      <div className="hashtag">
-                        <span>#пилатес</span>
-                      </div>
-                    </li>
-                    <li className="user-card__hashtag-item">
-                      <div className="hashtag">
-                        <span>#любитель</span>
-                      </div>
-                    </li>
-                  </ul>
-                  <button className="btn user-card__btn" type="button">
-                    Добавить в друзья
-                  </button>
+              {isUserCoach ? (
+                <div className="user-card__container">
+                  <UserInfo />
+                  <UserInfoWorkouts />
                 </div>
-                <div className="user-card__gallary">
-                  <ul className="user-card__gallary-list">
-                    <li className="user-card__gallary-item">
-                      <img
-                        src="img/content/user-card-photo1.jpg"
-                        srcSet="img/content/user-card-photo1@2x.jpg 2x"
-                        width={334}
-                        height={573}
-                        alt="photo1"
-                      />
-                    </li>
-                    <li className="user-card__gallary-item">
-                      <img
-                        src="img/content/user-card-photo2.jpg"
-                        srcSet="img/content/user-card-photo2@2x.jpg 2x"
-                        width={334}
-                        height={573}
-                        alt="photo2"
-                      />
-                    </li>
-                  </ul>
-                </div>
-              </div>
+              ) : (
+                <UserInfo />
+              )}
             </section>
           </div>
         </div>
