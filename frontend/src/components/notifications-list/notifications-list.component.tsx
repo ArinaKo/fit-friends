79d0bccac25c8049d9805599def8c54fc3171821
Arina <arinakovalenco7@gmail.com>
2024-05-13 12:46:12 +0300

    Добавит компонент NotificationsList

---
+++ frontend/src/components/notifications-list/notifications-list.component.tsx
@@ -0,0 +1,35 @@
+import { useAppSelector } from '../../hooks';
+import { getNotifications } from '../../store';
+import NotificationCard from './notification-card.component';
+import cn from 'classnames';
+
+function NotificationsList(): JSX.Element {
+  const notifications = useAppSelector(getNotifications);
+
+  return (
+    <li
+      className={cn('main-nav__item main-nav__item--notifications', {
+        'is-notifications': notifications.length !== 0,
+      })}
+    >
+      <button className="main-nav__link" aria-label="Уведомления">
+        <svg width={14} height={18} aria-hidden="true">
+          <use xlinkHref="#icon-notification" />
+        </svg>
+      </button>
+      <div className="main-nav__dropdown">
+        <p className="main-nav__label">Оповещения</p>
+        <ul className="main-nav__sublist">
+          {notifications.map((notification) => (
+            <NotificationCard
+              notification={notification}
+              key={`notification-${notification.id}`}
+            />
+          ))}
+        </ul>
+      </div>
+    </li>
+  );
+}
+
+export default NotificationsList;
