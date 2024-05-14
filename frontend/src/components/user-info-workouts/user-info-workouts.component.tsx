4350fd7c9bc35ca4ea64920fbf5a1cf957776a43
Arina <arinakovalenco7@gmail.com>
2024-05-14 16:32:53 +0300

    Дополнит компонент UserInfoWorkouts

--- frontend/src/components/user-info-workouts/user-info-workouts.component.tsx
+++ frontend/src/components/user-info-workouts/user-info-workouts.component.tsx
@@ -2,10 +2,18 @@ import { useEffect, useRef } from 'react';
 import { SliderConfig, SlidesAmount } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import {
+  createWorkoutRequestAction,
   getCoachDataAction,
+  getSubscriptionStatus,
+  getUserId,
+  getUserIsReady,
   getUserWorkouts,
   isCoachInfoActual,
   isCoachWorkoutsLoading,
+  isUserAFriend,
+  isUserCoach,
+  subscribeToCoachAction,
+  unsubscribeFromCoachAction,
 } from '../../store';
 import {
   WorkoutCard,
@@ -18,7 +26,12 @@ import Slider from 'react-slick';
 
 function UserInfoWorkouts(): JSX.Element {
   const dispatch = useAppDispatch();
+  const isCoach = useAppSelector(isUserCoach);
+  const userId = useAppSelector(getUserId);
   const workouts = useAppSelector(getUserWorkouts);
+  const isReady = useAppSelector(getUserIsReady);
+  const isFriend = useAppSelector(isUserAFriend);
+  const subscriptionStatus = useAppSelector(getSubscriptionStatus);
   const isDataActual = useAppSelector(isCoachInfoActual);
   const isDataLoading = useAppSelector(isCoachWorkoutsLoading);
   const sliderRef = useRef<Slider>(null);
@@ -37,6 +50,18 @@ function UserInfoWorkouts(): JSX.Element {
   if (isDataLoading) {
     return <UIBlocker />;
   }
+
+  const handleRequestButtonClick = () => {
+    dispatch(createWorkoutRequestAction(userId));
+  };
+
+  const handleSubscribeButtonClick = () => {
+    if (subscriptionStatus) {
+      dispatch(unsubscribeFromCoachAction(userId));
+      return;
+    }
+    dispatch(subscribeToCoachAction(userId));
+  };
 
   return (
     <div className="user-card__training">
@@ -60,28 +85,37 @@ function UserInfoWorkouts(): JSX.Element {
         ))}
       </Slider>
       <form className="user-card__training-form">
-        <button className="btn user-card__btn-training" type="button">
-          Хочу персональную тренировку
-        </button>
-        <div className="user-card__training-check">
-          <div className="custom-toggle custom-toggle--checkbox">
-            <label>
-              <input
-                type="checkbox"
-                defaultValue="user-agreement-1"
-                name="user-agreement"
-              />
-              <span className="custom-toggle__icon">
-                <svg width={9} height={6} aria-hidden="true">
-                  <use xlinkHref="#arrow-check" />
-                </svg>
-              </span>
-              <span className="custom-toggle__label">
-                Получать уведомление на почту о новой тренировке
-              </span>
-            </label>
+        {isReady && !isCoach && isFriend ? (
+          <button
+            className="btn user-card__btn-training"
+            type="button"
+            onClick={handleRequestButtonClick}
+          >
+            Хочу персональную тренировку
+          </button>
+        ) : undefined}
+        {!isCoach ? (
+          <div className="user-card__training-check">
+            <div className="custom-toggle custom-toggle--checkbox">
+              <label>
+                <input
+                  type="checkbox"
+                  name="subscription"
+                  checked={subscriptionStatus}
+                  onChange={handleSubscribeButtonClick}
+                />
+                <span className="custom-toggle__icon">
+                  <svg width={9} height={6} aria-hidden="true">
+                    <use xlinkHref="#arrow-check" />
+                  </svg>
+                </span>
+                <span className="custom-toggle__label">
+                  Получать уведомление на почту о новой тренировке
+                </span>
+              </label>
+            </div>
           </div>
-        </div>
+        ) : undefined}
       </form>
     </div>
   );
